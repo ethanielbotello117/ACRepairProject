008c60b74d9442c15b0df00059146c68ce5dcf21
Ethaniel Botello <Ebotel200@west-mec.local>
2021-08-27 15:24:57 -0700

    test

--- src/Pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from "./Home";
-import Error from "./Error";
-import Gallery from "./Gallery";
-import About from "./About";
-import Contact from "./Contact";
-
-export {Home, Error, Gallery, About, Contact};
