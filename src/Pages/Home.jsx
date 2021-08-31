d72676186f0e917aae8427c82133d3b7a35982c7
Ethaniel Botello <Ebotel200@west-mec.local>
2021-08-31 14:03:32 -0700

    home page done and fully linked with more compact card maker

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,30 +1,29 @@
-import { FaImages, FaPhone, FaInfoCircle } from 'react-icons/fa'
-import React, { useState, useEffect } from 'react'
+// import { FaImages, FaPhone, FaInfoCircle, FaHome } from 'react-icons/fa'
+import { boxLinks } from "../util/consts";
+import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-    return (
-        <div className="main">
-            <div className="backdrop"></div>
-            <div className="cards">
-                <div className="img-container">
-                    <h1 className="icon"><FaInfoCircle /></h1>
-                </div>
-                <h2 className="title">ABOUT</h2>
+  return (
+    <div className="main">
+      <div className="bg"></div>
+      {boxLinks
+        .filter((link) => link.text !== "Error")
+        .map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <div className="cards" key={id}>
+              <Link className="img-container" to={url}>
+                <h1 className="icon">{icon}</h1>
+              </Link>
+              <Link to={url}>
+                <h2 className="title">{text}</h2>
+              </Link>
             </div>
-            <div className="cards">
-                <div className="img-container">
-                    <h1 className="icon"><FaImages /></h1>
-                </div>
-                <h2 className="title">GALLERY</h2>
-            </div>
-            <div className="cards">
-                <div className="img-container">
-                    <h1 className="icon"><FaPhone /></h1>
-                </div>
-                <h2 className="title">CONTACT</h2>
-            </div>
-        </div>
-    )
-}
+          );
+        })}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/util/consts.js
+++ src/util/consts.js
@@ -1,4 +1,4 @@
-import { FaFacebook,  FaTwitter, FaReddit } from 'react-icons/fa'
+import { FaFacebook,  FaTwitter, FaReddit, FaImages, FaPhone, FaInfoCircle, FaHome } from 'react-icons/fa'
 import React from 'react'
 import { Home, Gallery, Error, About, Contact } from '../Pages'
 
@@ -8,25 +8,59 @@ export const links = [
         id: 1,
         url: '/',
         text: 'Home',
-        page: <Home />
+        page: <Home />,
+        icon: <FaHome />
     },
     {
         id: 2,
         url: '/about',
         text: 'About',
-        page: <About />
+        page: <About />,
+        icon: <FaInfoCircle />
     },
     {
         id: 3,
         url: '/gallery',
         text: 'Gallery',
-        page: <Gallery />
+        page: <Gallery />,
+        icon: <FaImages />
     },
     {
         id: 4,
         url: '/contact',
         text: 'Contact',
-        page: <Contact />
+        page: <Contact />,
+        icon: <FaPhone />
+    },
+    {
+        id: 5,
+        url: '*',
+        text: 'Error',
+        page: <Error />,
+    },
+];
+
+export const boxLinks = [
+    {
+        id: 2,
+        url: '/about',
+        text: 'ABOUT',
+        page: <About />,
+        icon: <FaInfoCircle />
+    },
+    {
+        id: 3,
+        url: '/gallery',
+        text: 'GALLERY',
+        page: <Gallery />,
+        icon: <FaImages />
+    },
+    {
+        id: 4,
+        url: '/contact',
+        text: 'CONTACT',
+        page: <Contact />,
+        icon: <FaPhone />
     },
     {
         id: 5,
